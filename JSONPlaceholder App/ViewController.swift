e06436f2638d463ec3f71276bedd5bf3eeaa8c32
mryusuf <muhamadyusufindra@gmail.com>
2021-07-10 19:50:31 +0700

    Initial Commit

---
+++ JSONPlaceholder App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSONPlaceholder App
+//
+//  Created by Indra Permana on 10/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
